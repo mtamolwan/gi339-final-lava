8c41c531d26b01c023c1fefb4554a7c1449e1ad9
NattaponKhuenkeaw <nattapon.khuenkeaw@gmail.com>
2026-04-15 21:01:09 +0700

    V1

---
+++ Assets/Script/GameOverManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class GameOverManager : MonoBehaviour
+{
+    public GameObject gameOverPanel; 
+
+    void Start()
+    {
+        
+        Time.timeScale = 1f;
+        if (gameOverPanel != null)
+        {
+            gameOverPanel.SetActive(false);
+        }
+    }
+
+    public void SetupGameOver()
+    {
+        gameOverPanel.SetActive(true); 
+        Time.timeScale = 0f;          
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f; 
+       
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Script/PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public GameOverManager gameOverManager; 
+
+    
+
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            gameOverManager.SetupGameOver();
+        }
+    }
+    
+}
