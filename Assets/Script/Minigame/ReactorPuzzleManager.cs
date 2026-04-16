154a86ce4d78aa15ed5099ae50dac260d08c3905
Fighttttz <nantawut.buat@bumail.net>
2026-04-17 04:58:34 +0700

    Squashed commit of the following:
    commit 36f5569bee2ec6a0595f944c75422f7a1c936292
    Author: Fighttttz <nantawut.buat@bumail.net>
    Date:   Fri Apr 17 04:57:17 2026 +0700
    
        LV8 Finish
    
    commit 62e9610eecd51703ccfa98caf005211ee957878a
    Author: Fighttttz <nantawut.buat@bumail.net>
    Date:   Fri Apr 17 04:16:52 2026 +0700
    
        เหยียบน้ำขิต LV8
    
    commit 1a58a6a740cf83552e6a7404dba3ab04cc742d82
    Author: Fighttttz <nantawut.buat@bumail.net>
    Date:   Fri Apr 17 03:34:46 2026 +0700
    
        Squashed commit of the following:
    
        commit 3022e5fcb22e1bc4f5417c8100adb60797729abb
        Author: Fighttttz <nantawut.buat@bumail.net>
        Date:   Fri Apr 17 03:22:25 2026 +0700
    
            แก้2-เข้าให้กูเถอะ
    
    commit 2181f508128167726855a6fb0a9f2a1081a404eb
    Author: Fighttttz <nantawut.buat@bumail.net>
    Date:   Fri Apr 17 03:30:45 2026 +0700
    
        Revert "ขอร้องเข้าเถอะ LV8"
    
        This reverts commit bc219574e2cb07a988bf16f022914205a80fe27d.
    
    commit bc219574e2cb07a988bf16f022914205a80fe27d
    Author: Fighttttz <nantawut.buat@bumail.net>
    Date:   Fri Apr 17 03:24:08 2026 +0700
    
        ขอร้องเข้าเถอะ LV8
    
    commit ad76b04b3b4ae51eff8960b1b959eb75624bcfe9
    Merge: 9a25b69 0f0b6ce
    Author: Fighttttz <nantawut.buat@bumail.net>
    Date:   Fri Apr 17 03:14:39 2026 +0700
    
        Squashed commit of the following:
    
        commit 0f0b6ce8e2a767a5237370b36ecec64e4a94f7fa
        Author: Fighttttz <nantawut.buat@bumail.net>
        Date:   Fri Apr 17 02:48:19 2026 +0700
    
            แก้
        Revert "Grey Box น่าจะเสร็จละ"
    
        This reverts commit 35a9abf3084154b1626a4e3bc7dc8321f420b793.
    
    commit 9a25b69354626ec3f6c0f3d4622ad3a8c7c3e501
    Author: Fighttttz <nantawut.buat@bumail.net>
    Date:   Fri Apr 17 02:40:46 2026 +0700
    
        Revert "Merge branch '08'"
    
        This reverts commit ae7afa436164787199081758d38e614792442849, reversing
        changes made to ec3b4bf083fa68749ac42d41ee65dbef1fc5e957.
    
    commit ae7afa436164787199081758d38e614792442849
    Merge: ec3b4bf 35a9abf
    Author: Fighttttz <nantawut.buat@bumail.net>
    Date:   Fri Apr 17 02:28:20 2026 +0700
    
        Merge branch '08'
    
    commit ec3b4bf083fa68749ac42d41ee65dbef1fc5e957
    Author: NattaponKhuenkeaw <nattapon.khuenkeaw@gmail.com>
    Date:   Thu Apr 16 11:05:41 2026 +0700
    
        fix_lvl_05
    
    commit 77420b85312765097d32f99214596849f6cecea7
    Author: Panisara2509 <panisara.angs@bumail.net>
    Date:   Thu Apr 16 10:50:53 2026 +0700
    
        level5
    
    commit e2980ab9bb87686358dd2fab1281f0e9ab18bd4e
    Merge: d1c2695 5570361
    Author: NattaponKhuenkeaw <nattapon.khuenkeaw@gmail.com>
    Date:   Thu Apr 16 09:17:44 2026 +0700
    
        Merge branch '06'
    
    commit 5570361f28b00c9660c5881bf2dc544e9ccb997d
    Author: NattaponKhuenkeaw <nattapon.khuenkeaw@gmail.com>
    Date:   Thu Apr 16 04:51:25 2026 +0700
    
        V2
    
    commit 8c41c531d26b01c023c1fefb4554a7c1449e1ad9
    Author: NattaponKhuenkeaw <nattapon.khuenkeaw@gmail.com>
    Date:   Wed Apr 15 21:01:09 2026 +0700
    
        V1
    
    commit d1c2695a8f9dd9f9218b00f2660a595af6da4118
    Author: Panisara2509 <panisara.angs@bumail.net>
    Date:   Fri Apr 10 10:31:38 2026 +0700
    
        Update lvl_5_yard scene
    
    commit e4558cadbbcaccbf3f7374ab190bed834fb870f3
    Merge: 94e1465 24663d1
    Author: peach4812 <malisa.khan@bumail.net>
    Date:   Wed Apr 8 02:25:44 2026 +0700
    
        DeleteMeterial
    
    commit 24663d1752e4336af33a409beaa2eec15a54e376
    Author: peach4812 <malisa.khan@bumail.net>
    Date:   Wed Apr 8 02:20:26 2026 +0700
    
        PlatformToilet
    
    commit 94e14659a7e1ac971f7a37f8acb34ebdc96e95c4
    Author: MalisaK <malisa.khan@bumail.net>
    Date:   Tue Apr 7 22:47:09 2026 +0700
    
        props
    
    commit 17804cb6b937dcfca7cb5ee961e31930de7f2c9a
    Author: MalisaK <malisa.khan@bumail.net>
    Date:   Tue Apr 7 21:59:15 2026 +0700
    
        PlatformPath2
    
    commit ba37dc4c112d19df60b447f54c5e4ac22719489b
    Author: MalisaK <malisa.khan@bumail.net>
    Date:   Tue Apr 7 21:44:22 2026 +0700
    
        PlatformPath1

---
+++ Assets/Script/Minigame/ReactorPuzzleManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class ReactorPuzzleManager : MonoBehaviour
+{
+    public int totalButtons = 3;
+    private int currentStep = 0;
+    public bool isPuzzleComplete = false;
+
+    public void ButtonStepped(int id)
+    {
+        if (isPuzzleComplete) return;
+
+        if (id == currentStep)
+        {
+            currentStep++;
+            Debug.Log("กดถูกลำดับ! ขั้นถัดไป: " + currentStep);
+
+            if (currentStep >= totalButtons)
+            {
+                isPuzzleComplete = true;
+                Debug.Log("Puzzle เสร็จสิ้น! ไปซ่อมเครื่องปฏิกรณ์ได้");
+            }
+        }
+        else
+        {
+            Debug.Log("ลำดับผิด! รีเซ็ตใหม่ทั้งหมด");
+            ResetPuzzle();
+        }
+    }
+
+    void ResetPuzzle()
+    {
+        currentStep = 0;
+        // สั่งให้ทุกปุ่มในฉากรีเซ็ตตัวเอง
+        ReactorButton[] buttons = FindObjectsByType<ReactorButton>(FindObjectsSortMode.None);
+        foreach (ReactorButton b in buttons)
+        {
+            b.ResetButton();
+        }
+    }
+}
