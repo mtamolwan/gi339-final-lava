ff4739ef477374e5f7ce786cc3f111295c18cab2
phuwynn <phuwynn.dees@bumail.net>
2026-04-17 18:03:36 +0700

    เพิ่มระบบกับดักและจุดเปลี่ยนด่าน

---
+++ Assets/Scrips/FallingTrap.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+public class FallingTrap : MonoBehaviour
+{
+    public float fallDelay = 0.5f; 
+    private Rigidbody rb;
+    private bool isFalling = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // เปลี่ยนจาก OnCollisionEnter เป็น OnTriggerEnter
+    // และเปลี่ยน Collision เป็น Collider
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("มีคนมาเหยียบกล่อง! ชื่อ: " + other.gameObject.name + " ป้ายTagคือ: " + other.gameObject.tag);
+
+        if (other.CompareTag("Player") && !isFalling)
+        {
+            StartCoroutine(DropPlatform());
+        }
+    }
+
+    IEnumerator DropPlatform()
+    {
+        isFalling = true;
+        yield return new WaitForSeconds(fallDelay);
+        rb.isKinematic = false; 
+        rb.velocity = new Vector3(0, -20f, 0);
+        Destroy(gameObject, 3f); 
+    }
+}
