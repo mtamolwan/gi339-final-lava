6ea336607169ff899fcfca984c795bf00f9c6106
phuwynn <phuwynn.dees@bumail.net>
2026-05-02 13:04:27 +0700

    fix text e at breaker, fix jump scare point, set scene (finish))

--- Assets/Scripts/WaterKillZone.cs
+++ Assets/Scripts/WaterKillZone.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class WaterKillZone : MonoBehaviour
 {
+    [Header("ลากก้อน BGM มาใส่ช่องนี้")]
+    public GameObject bgmObject;
+
     void OnTriggerEnter(Collider other)
     {
         // ตรวจสอบว่าคนที่ตกลงมาโดนน้ำคือผู้เล่นใช่ไหม
@@ -9,7 +12,12 @@ public class WaterKillZone : MonoBehaviour
         {
             Debug.Log("Game Over");
             
-        
+            // 🛑 สั่งปิดออบเจกต์ BGM ทิ้ง เสียงทุกอย่างที่แปะอยู่จะดับสนิท!
+            if (bgmObject != null)
+            {
+                bgmObject.SetActive(false);
+            }
+            
             FindFirstObjectByType<GameOverManager>().SetupGameOver();
         }
     }
